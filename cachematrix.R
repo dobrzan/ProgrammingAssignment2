76de97e7963529d686de35643462d50804752320
dobrzan <jaroslaw.dobrzanski@gmail.com>
2015-01-22 21:21:54 +0100

    Minor changes to comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,11 @@
-## -----------------
 ## Functions below are part of an example on how to 
 ## cache potentially time consuming computations
-## -----------------
 
-## Creates a special matrix object that can cache its inverse.
-
-## 'x' is a matrix for which its inverse is cached
 makeCacheMatrix <- function(x = matrix()) {
+    # Creates a special matrix object that can cache its inverse.
+    #
+    # Args:
+    #  x: a matrix for which its inverse is cached
     s <- NULL
     set <- function(y) {
         x <<- y
@@ -21,12 +20,14 @@ makeCacheMatrix <- function(x = matrix()) {
          getSolve = getSolve)    
 }
 
-## Returns computed the inverse of the special matrix created with 
-## 'makeCacheMatrix' function. The inverse is returned from cache (if possible),
-## or calculated (and cached) when executed.
-
-## 'x' is a special matrix object that can cache its inverse
 cacheSolve <- function(x, ...) {
+    # Returns computed the inverse of the special matrix created with 
+    # makeCacheMatrix function. 
+    # The inverse is returned from cache (if possible),
+    # or calculated (and cached) when executed.
+    #
+    # Args:
+    #  x: a special matrix object that can cache its inverse
     stopifnot(!is.na(x))
     s <- x$getSolve()
     if (!is.null(s)) {
